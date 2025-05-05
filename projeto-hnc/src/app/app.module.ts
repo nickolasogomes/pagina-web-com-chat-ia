d1d0224f6545873b6dcb3f8926a4890d390cc81b
simaonarf <simaofranciel1@gmail.com>
2025-05-04 22:50:19 -0300

    up container

--- projeto-hnc/src/app/app.module.ts
+++ projeto-hnc/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
